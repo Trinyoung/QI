a4f8461e00f7b80d30344799a9f0c56043bc0961
luqingyang <trinyoung@163.com>
2019-03-21 18:33:42 +0800

    add config

---
+++ config/index.js
@@ -0,0 +1,6 @@
+const test = require('config.test');
+const prod = require('config.prod');
+const dev = require('config.dev')
+module.exports = {
+  
+}
--- test.js
+++ test.js
@@ -1,6 +1,7 @@
-var obj = {name: 'lqy', age: 23};
-console.log(JSON.stringify(obj));
-var buffer = new Buffer(JSON.stringify(obj));
-const buf = buffer.toString();
-console.log(JSON.parse(buf));
-module.exports = buf;
+// var obj = {name: 'lqy', age: 23};
+// console.log(JSON.stringify(obj));
+// var buffer = new Buffer(JSON.stringify(obj));
+// const buf = buffer.toString();
+// console.log(JSON.parse(buf));
+// module.exports = buf;
+
