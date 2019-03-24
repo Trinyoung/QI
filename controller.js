eb89ee58a507de9ec4276021e3dfa0c5b71c2b88
Trinyoung <Trinyoung@163.com>
2019-03-24 15:28:08 +0800

    fix controller

--- controller.js
+++ controller.js
@@ -1,12 +1,13 @@
 const saveData = require('./service/save_data');
-const logger = require('./util/log');
+const { logger, errLog } = require('./util/log');
+// const errLog = require('./util/log');
 module.exports = async (data) => {
   let result;
   try {
     result = await saveData.save(data);
     logger.info(`insert data successfully!`);
   } catch (err) {
-    logger.error(`insert data error: ${err}`);
+    errLog.error(`insert data error: ${err}`);
   }
   return result;
 }
