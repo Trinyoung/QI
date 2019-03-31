1f5469f9be7c7015bb0152af832043d99a6d044a
Trinyoung <Trinyoung@163.com>
2019-03-31 23:47:24 +0800

    fix save_data

--- service/save_data.js
+++ service/save_data.js
@@ -9,18 +9,19 @@ class Save {
         data = Object.assign({
             created_at: now.getTime() / 1000,
             updated_at: now.getTime() / 1000
-        },data);
+        }, data);
         const qi = new model(data);
-        if (!data.nid) {
+        if (!newData.nid) {
             errLog.error('cannot find node_id');
             return;
         }
+        // {"nid":825307184,"struct":{"eth0":1158938639,"eth1":36250219,"ppp":454}}
         try {
             const result = await qi.save();
             logger.debug('insert data successfully!');
             return result;
         } catch (e) {
-            errLog.error()
+            errLog.error(`insert data error: ${e}`);
         }
     }
 }
