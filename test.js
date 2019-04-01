0af7a73891bf9418679514511f1f3e7c20d82a10
trinyoung <trinyoung@163.com>
2019-04-01 14:18:32 +0800

    delete test

--- test.js
+++
@@ -1,7 +0,0 @@
-// var obj = {name: 'lqy', age: 23};
-// console.log(JSON.stringify(obj));
-// var buffer = new Buffer(JSON.stringify(obj));
-// const buf = buffer.toString();
-// console.log(JSON.parse(buf));
-// module.exports = buf;
-
