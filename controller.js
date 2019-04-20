09fc79a0594ecd4b5ae78669ac7a0a2a10fc0766
Trinyoung <Trinyoung@163.com>
2019-04-20 17:19:01 +0800

    fix controller

--- controller.js
+++ controller.js
@@ -2,5 +2,5 @@ const saveData = require('./service/save_data');
 const { logger, errLog } = require('./util/log');
   // TODO: 少传了？  await controller(msg, rinfo.address, rinfo.port);
 module.exports = async (data) => {
-  await saveData.save(data);
+  await saveData.save(data, address, port);
 }
