b748407e8424581e3186b4a9e9025adcf77eb9bd
Trinyoung <Trinyoung@163.com>
2019-05-17 14:59:01 +0800

    fix config

--- config/config.prod.js
+++ config/config.prod.js
@@ -1,6 +1,7 @@
 module.exports = {
   mongo: {
-    url: "mongodb://127.0.0.1:27017/qi"
+    // url: "mongodb://129.28.84.132:27017/qi"
+    url: "mongodb://127.0.0.1:27017/qi?gssapiServiceName=mongodb"
   },
   port: 41234
 }
