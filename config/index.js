d07a597a422631affabff702e8ce1fc8425f1bc6
Trinyoung <Trinyoung@163.com>
2019-05-20 09:56:48 +0800

    fix  address

--- config/index.js
+++ config/index.js
@@ -2,11 +2,13 @@ const test = require('./config.test');
 const prod = require('./config.prod');
 const dev = require('./config.dev');
 const base = require('./config.default');
+const { logger } = require('../util/log');
 module.exports = (()=>{
   const env = process.env.NODE_ENV;
   Object.setPrototypeOf(test, base);
   Object.setPrototypeOf(prod, base);
   Object.setPrototypeOf(dev, base);
+  logger.info(`server listening env:${env}`);
   let config;
   switch (env){
     case 'test':
--- controller.js
+++ controller.js
@@ -1,6 +1,6 @@
 const saveData = require('./service/save_data');
 const { logger, errLog } = require('./util/log');
   // TODO: 少传了？  await controller(msg, rinfo.address, rinfo.port);
-module.exports = async (data) => {
+module.exports = async (data, address, port) => {
   await saveData.save(data, address, port);
 }
